815d0cf5a8041da4fcd9cd5fc65508cfb9f8271d
Lutfee Dorloh <lutfee2017@gmail.com>
2026-02-11 23:28:24 +0700

    feat: Implement authentication middleware for user access control and configure API rewrites for environment-specific backend routing.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'production'
+            ? `${process.env.NEXT_PUBLIC_API_URL_PRODUCTION}/:path*`
+            : `${process.env.NEXT_PUBLIC_API_URL_DEV}/:path*`,
+      },
+    ];
+  },
 };
-
-export default nextConfig;
